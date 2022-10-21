e01f6b8d56cd7ff68e971a2bb33b0aed9468514e
Shubhajit123Anudip <111556510+Shubhajit123Anudip@users.noreply.github.com>
2022-10-21 12:06:19 +0530

    Update FloatingPoint.java

--- FloatingPoint.java
+++ FloatingPoint.java
@@ -14,12 +14,6 @@ public class FloatingPoint {
 		        System.out.print("Input floating-point another number: ");
 		        double y = in.nextDouble();
 
-		        x = Math.round(x * 1000);
-		        x = x / 1000;
-
-		        y = Math.round(y * 1000);
-		        y = y / 1000;
-
 		        if (x == y)
 		        {
 		            System.out.println("They are the same up to three decimal places");
