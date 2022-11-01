fa6a270035f95ad3b08154ca9efac2295e6998c7
Shubhajit Maji <111556510+Shubhajit123Anudip@users.noreply.github.com>
2022-11-01 12:42:39 +0530

    Add files via upload

---
+++ 1-11-2022/src/Assginment/Odd.java
@@ -0,0 +1,14 @@
+package Assginment;
+
+public class Odd {
+
+	public static void main(String[] args) {
+		int arr[] = { 1, 2, 3, 4, 5, 6 }; // initialize the array
+		for (int i = 0; i < arr.length; i = i + 2) { // for loop
+			System.out.println("The Array present in odd position are: " + arr[i]);
+		}
+	
+
+	}
+
+}
